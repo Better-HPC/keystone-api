7394ca218cca31f0b107586ea4c9c2aa48aaeaab
Daniel Perrefort <djperrefort@gmail.com>
2025-03-14 14:29:45 -0400

    Adds support for nested membership data in user read operations

--- keystone_api/apps/users/serializers.py
+++ keystone_api/apps/users/serializers.py
@@ -23,15 +23,25 @@ __all__ = [
 class TeamSerializer(serializers.ModelSerializer):
     """Object serializer for the `Team` model."""
 
+    membership = serializers.SerializerMethodField()
+
     class Meta:
         """Serializer settings."""
 
         model = Team
-        fields = '__all__'
+        fields = ['id', 'name', 'is_active', 'membership']
+
+    def get_membership(self, obj: Team) -> dict:
+        """Return a mapping of usernames to user roles."""
+
+        return {mem.user.username: mem.role for mem in obj.teammembership_set.all()}
 
 
 class TeamMembershipSerializer(serializers.ModelSerializer):
     """Object serializer for the `TeamMembership` model."""
+
+    user = serializers.SerializerMethodField()
+    team = serializers.SerializerMethodField()
 
     class Meta:
         """Serializer settings."""
@@ -39,9 +49,21 @@ class TeamMembershipSerializer(serializers.ModelSerializer):
         model = TeamMembership
         fields = '__all__'
 
+    def get_user(self, obj: TeamMembership) -> str:
+        """Return the user's username."""
+
+        return obj.user.username
+
+    def get_team(self, obj: TeamMembership) -> str:
+        """Return the team's name."""
+
+        return obj.team.name
+
 
 class PrivilegedUserSerializer(serializers.ModelSerializer):
     """Object serializer for the `User` model including administrative fields."""
+
+    membership = serializers.SerializerMethodField()
 
     class Meta:
         """Serializer settings."""
@@ -65,6 +87,11 @@ class PrivilegedUserSerializer(serializers.ModelSerializer):
 
         return super().validate(attrs)
 
+    def get_membership(self, obj: User) -> dict:
+        """Return a mapping of team names to membership roles."""
+
+        return {mem.team.name: mem.role for mem in obj.teammembership_set.all()}
+
 
 class RestrictedUserSerializer(PrivilegedUserSerializer):
     """Object serializer for the `User` class with administrative fields marked as read only."""
@@ -74,7 +101,7 @@ class RestrictedUserSerializer(PrivilegedUserSerializer):
 
         model = User
         fields = '__all__'
-        read_only_fields = ['is_active', 'is_staff', 'is_ldap_user', 'date_joined', 'last_login', 'profile_image']
+        read_only_fields = ['is_active', 'is_staff', 'is_ldap_user', 'date_joined', 'last_login', 'profile_image', 'membership']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data: dict) -> None:
