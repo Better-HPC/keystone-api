b7fb1ef9383c5cb403124f6546775edfd6bd2dac
Daniel Perrefort <djperrefort@gmail.com>
2025-04-02 11:57:38 -0400

    Adds nested read-only fields to logging app

--- keystone_api/apps/logging/serializers.py
+++ keystone_api/apps/logging/serializers.py
@@ -5,12 +5,16 @@ representations in a manner that is suitable for use by RESTful endpoints.
 They encapsulate object serialization, data validation, and database object
 creation.
 """
-
+from django.contrib.auth import get_user_model
 from rest_framework import serializers
 
 from .models import *
 
 __all__ = ['AppLogSerializer', 'RequestLogSerializer', 'TaskResultSerializer']
+
+from ..users.serializers import UserSummarySerializer
+
+User = get_user_model()
 
 
 class AppLogSerializer(serializers.ModelSerializer):
@@ -25,6 +29,8 @@ class AppLogSerializer(serializers.ModelSerializer):
 
 class RequestLogSerializer(serializers.ModelSerializer):
     """Object serializer for the `RequestLog` class."""
+
+    _user = UserSummarySerializer(source='user', read_only=True)
 
     class Meta:
         """Serializer settings."""
