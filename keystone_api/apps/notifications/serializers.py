1fd9bc043d13bc33e71697c0be7c0083e7470def
Daniel Perrefort <djperrefort@gmail.com>
2025-05-14 22:24:07 -0400

    Adds notification serializers

---
+++ keystone_api/apps/notifications/serializers.py
@@ -0,0 +1,36 @@
+"""Serializers for casting database models to/from JSON and XML representations.
+
+Serializers handle the casting of database models to/from HTTP compatible
+representations in a manner that is suitable for use by RESTful endpoints.
+They encapsulate object serialization, data validation, and database object
+creation.
+"""
+
+from rest_framework import serializers
+
+from .models import *
+
+__all__ = [
+    'NotificationSerializer',
+    'PreferenceSerializer',
+]
+
+
+class NotificationSerializer(serializers.ModelSerializer):
+    """Object serializer for the `Notification` class."""
+
+    class Meta:
+        """Serializer settings."""
+
+        model = Notification
+        fields = '__all__'
+
+
+class PreferenceSerializer(serializers.ModelSerializer):
+    """Object serializer for the `Preference` class."""
+
+    class Meta:
+        """Serializer settings."""
+
+        model = Preference
+        fields = '__all__'
