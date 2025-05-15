55520d43186d2e1973a8a3c72a96fa4ff1f38e09
Daniel Perrefort <djperrefort@gmail.com>
2025-05-14 22:28:57 -0400

    Adds notification urls

---
+++ keystone_api/apps/notifications/urls.py
@@ -0,0 +1,11 @@
+"""URL routing for the parent application."""
+
+from rest_framework.routers import DefaultRouter
+
+from .views import *
+
+app_name = 'users'
+
+router = DefaultRouter()
+router.register('notifications', NotificationViewSet)
+router.register('preferences', PreferenceViewSet)
