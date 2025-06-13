fce089b7e92adb32d693e6b88b528c7d0353e44b
Daniel Perrefort <djperrefort@gmail.com>
2025-06-12 23:13:28 -0400

    Derive custom email backend from BaseEmailBackend class

--- keystone_api/plugins/email.py
+++ keystone_api/plugins/email.py
@@ -1,31 +1,47 @@
-import os
+from pathlib import Path
 
-from django.core.mail.backends.filebased import EmailBackend
+from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.core.mail.backends.base import BaseEmailBackend
 from django.utils.text import slugify
 
 
-class EmlFileBasedEmailBackend(EmailBackend):
+class EmlFileBasedEmailBackend(BaseEmailBackend):
 
-    def __get_filename(self, message):
+    def __init__(self, *args, **kwargs) -> None:
+
+        super().__init__(*args, **kwargs)
+
+        self._output_dir = getattr(settings, 'EMAIL_FILE_PATH')
+        if self._output_dir is None:
+            raise ImproperlyConfigured('EMAIL_FILE_PATH must be set to use EmlFileBasedEmailBackend.')
+
+        if not self._output_dir.exists():
+            raise RuntimeError(f'Directory does not exist: {self._output_dir}')
+
+    def _generate_file_path(self, message) -> Path:
 
         # Generate a file name from the message subject
         filename = slugify(message.subject)
-        full_path = os.path.join(self.file_path, f"{filename}.eml")
+        return self._output_dir / f"{filename}.eml"
 
-        # Avoid overwriting existing files by appending a numeric suffix if necessary
-        counter = 1
-        while os.path.exists(full_path):
-            full_path = os.path.join(self.file_path, f"{filename}-{counter}.eml")
-            counter += 1
+    def write_message(self, message) -> None:
+        """Write an email message to disk.
 
-        return full_path
+        Args:
+            message: The message to write.
+        """
 
-    def write_message(self, message):
-
-        filename = self.__get_filename(message)
+        filename = self._generate_file_path(message)
         with open(filename, 'a') as out_file:
             out_file.write(message.message().as_string())
 
-    def send_messages(self, email_messages):
+    def send_messages(self, email_messages) -> None:
+        """Send a list of email messages.
+
+        Args:
+            email_messages: The messages to send.
+        """
+
         for message in email_messages:
             self.write_message(message)
