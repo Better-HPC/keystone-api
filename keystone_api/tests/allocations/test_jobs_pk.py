cb8147a2fccee0de340d2b371377106d16d195d6
Daniel Perrefort <djperrefort@gmail.com>
2025-06-23 21:20:59 -0400

    Adds per record permissions

--- keystone_api/tests/allocations/test_jobs_pk.py
+++ keystone_api/tests/allocations/test_jobs_pk.py
@@ -1,10 +1,10 @@
-"""Function tests for the `/allocations/jobs/` endpoint."""
+"""Function tests for the `/allocations/jobs/<pk>/` endpoint."""
 
 from rest_framework import status
 from rest_framework.test import APITestCase
 
 from apps.allocations.models import JobStats
-from apps.users.models import User
+from apps.users.models import Team, User
 from tests.utils import CustomAsserts, TeamScopedListFilteringTests
 
 
@@ -16,18 +16,24 @@ class EndpointPermissions(APITestCase, CustomAsserts):
     | User Status                | GET | HEAD | OPTIONS | POST | PUT | PATCH | DELETE | TRACE |
     |----------------------------|-----|------|---------|------|-----|-------|--------|-------|
     | Unauthenticated User       | 401 | 401  | 401     | 401  | 401 | 401   | 401    | 401   |
-    | Authenticated User         | 200 | 200  | 200     | 405  | 405 | 405   | 405    | 405   |
+    | Authenticated non-member   | 403 | 403  | 200     | 405  | 405 | 405   | 405    | 405   |
+    | Team member                | 200 | 200  | 200     | 405  | 405 | 405   | 405    | 405   |
     | Staff User                 | 200 | 200  | 200     | 405  | 405 | 405   | 405    | 405   |
     """
 
-    endpoint = '/allocations/jobs/'
+    endpoint_pattern = '/allocations/jobs/{pk}/'
     fixtures = ['testing_common.yaml']
 
     def setUp(self) -> None:
         """Load user accounts from test fixtures."""
 
-        self.generic_user = User.objects.get(username='generic_user')
+        self.team = Team.objects.get(name='Team 1')
+        self.jobstat = JobStats.objects.create(team=self.team)
+        self.endpoint = self.endpoint_pattern.format(pk=self.jobstat.pk)
+
         self.staff_user = User.objects.get(username='staff_user')
+        self.non_member = User.objects.get(username='generic_user')
+        self.team_member = User.objects.get(username='member_1')
 
     def test_unauthenticated_user_permissions(self) -> None:
         """Verify unauthenticated users cannot access resources."""
@@ -44,10 +50,26 @@ class EndpointPermissions(APITestCase, CustomAsserts):
             trace=status.HTTP_401_UNAUTHORIZED
         )
 
-    def test_authenticated_user_permissions(self) -> None:
-        """Verify authenticated users have read-only permissions."""
+    def test_non_team_member_permissions(self) -> None:
+        """Verify users cannot access records for a team they are not in."""
 
-        self.client.force_authenticate(user=self.generic_user)
+        self.client.force_authenticate(user=self.non_member)
+        self.assert_http_responses(
+            self.endpoint,
+            get=status.HTTP_403_FORBIDDEN,
+            head=status.HTTP_403_FORBIDDEN,
+            options=status.HTTP_200_OK,
+            post=status.HTTP_405_METHOD_NOT_ALLOWED,
+            put=status.HTTP_405_METHOD_NOT_ALLOWED,
+            patch=status.HTTP_405_METHOD_NOT_ALLOWED,
+            delete=status.HTTP_405_METHOD_NOT_ALLOWED,
+            trace=status.HTTP_405_METHOD_NOT_ALLOWED,
+        )
+
+    def test_team_member_permissions(self) -> None:
+        """Verify team members have read-only permissions against their own group records."""
+
+        self.client.force_authenticate(user=self.team_member)
         self.assert_http_responses(
             self.endpoint,
             get=status.HTTP_200_OK,
