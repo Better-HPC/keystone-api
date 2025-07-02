f8ca217efbc116ec8c1e6514a57744e498fb1c44
Daniel Perrefort <djperrefort@gmail.com>
2025-07-02 13:58:21 -0400

    Switches from whitenoise to servestatic

--- keystone_api/main/settings.py
+++ keystone_api/main/settings.py
@@ -150,7 +150,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
+    "servestatic.middleware.ServeStaticMiddleware",
     'auditlog.middleware.AuditlogMiddleware',
     'django_prometheus.middleware.PrometheusAfterMiddleware',
 ]
