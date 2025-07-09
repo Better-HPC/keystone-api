13986a1e1aa2481e59735eabc265729108ab6672
Daniel Perrefort <djperrefort@gmail.com>
2025-07-08 21:17:11 -0400

    Updates whoami unit tests

--- keystone_api/apps/authentication/tests/test_views/test_WhoAmIView.py
+++ keystone_api/apps/authentication/tests/test_views/test_WhoAmIView.py
@@ -11,23 +11,22 @@ User = get_user_model()
 
 
 class GetMethod(TestCase):
-    """Test HTTP request handling by the `get` method."""
+    """Test HTTP GET handling by the `get` method."""
 
     def setUp(self) -> None:
         """Create a view instance and HTTP request factory."""
 
+        self.view = WhoAmIView()
         self.factory = RequestFactory()
-        self.view = WhoAmIView.as_view()
         self.user = User.objects.create(username='testuser', password='password')
 
-    def test_get_authenticated_user(self) -> None:
-        """Verify user data is returned for an authenticated user."""
+    def test_authenticated_user(self) -> None:
+        """Verify authenticated users are returned their own metadata."""
 
         request = self.factory.get('/whoami/')
         request.user = self.user
+        expected_data = RestrictedUserSerializer(request.user).data
 
-        response = self.view(request)
-        expected_data = RestrictedUserSerializer(self.user).data
-
+        response = self.view.get(request)
         self.assertEqual(status.HTTP_200_OK, response.status_code)
         self.assertEqual(expected_data, response.data)
