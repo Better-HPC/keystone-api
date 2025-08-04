df716f7cb1b7a4c07e4e07cf282f7a61898cd102
Daniel Perrefort <djperrefort@gmail.com>
2025-08-04 19:26:56 -0400

    Moves unit tests into correct location

--- keystone_api/apps/notifications/tests/test_models/test_Preference.py
+++ keystone_api/apps/notifications/tests/test_models/test_Preference.py
@@ -150,3 +150,106 @@ class SetUserPreferenceMethod(TestCase):
         Preference.set_user_preference(user=self.user, notify_on_expiration=False)
         preference.refresh_from_db()
         self.assertFalse(preference.notify_on_expiration)
+
+
+class ShouldNotifyPastExpirationMethod(TestCase):
+    """Test the determination of whether an expiration notification should be issued."""
+
+    def setUp(self) -> None:
+        """Set up test data."""
+
+        self.user = User.objects.create_user(username='testuser', password='foobar123!')
+        self.team = Team.objects.create()
+        self.request = AllocationRequest.objects.create(team=self.team, expire=date.today())
+
+    @patch('apps.notifications.models.Notification.objects.filter')
+    def test_false_if_duplicate_notification(self, mock_notification_filter: Mock) -> None:
+        """Verify the return value is `False` if a notification has already been issued."""
+
+        mock_notification_filter.return_value.exists.return_value = True
+        with self.assertLogs('apps.allocations.tasks', level='DEBUG') as log:
+            self.assertFalse(should_notify_past_expiration(self.user, self.request))
+            self.assertRegex(log.output[-1], '.*Notification already sent.')
+
+    def test_false_if_disabled_in_preferences(self) -> None:
+        """Verify the return value is `False` if expiry notifications are disabled in preferences."""
+
+        Preference.objects.create(user=self.user, notify_on_expiration=False)
+        self.assertFalse(should_notify_past_expiration(self.user, self.request))
+
+    def test_true_if_new_notification(self) -> None:
+        """Verify the return value is `True` if a notification has not been issued yet."""
+
+        self.assertTrue(should_notify_past_expiration(self.user, self.request))
+
+
+class ShouldNotifyUpcomingExpirationMethod(TestCase):
+    """Test the determination of whether an expiry threshold notification should be issued."""
+
+    def setUp(self) -> None:
+        """Set up test data."""
+
+        self.user = User.objects.create_user(
+            username='testuser',
+            password='foobar123!',
+            date_joined=datetime(2020, 1, 1, tzinfo=timezone.utc)
+        )
+
+        self.team = Team.objects.create()
+        self.request = AllocationRequest.objects.create(team=self.team, expire=date.today() + timedelta(days=15))
+
+    def test_false_if_request_does_not_expire(self) -> None:
+        """Verify the return value is `False` if the request does not expire."""
+
+        self.request.expire = None
+        with self.assertLogs('apps.allocations.tasks', level='DEBUG') as log:
+            self.assertFalse(should_notify_upcoming_expiration(self.user, self.request))
+            self.assertRegex(log.output[-1], '.*Request does not expire.')
+
+    def test_false_if_request_already_expired(self) -> None:
+        """Verify the return value is `False` if the request has already expired."""
+
+        self.request.expire = date.today()
+        with self.assertLogs('apps.allocations.tasks', level='DEBUG') as log:
+            self.assertFalse(should_notify_upcoming_expiration(self.user, self.request))
+            self.assertRegex(log.output[-1], '.*Request has already expired.')
+
+    def test_false_if_no_threshold_reached(self) -> None:
+        """Verify the return value is `False` if no threshold is reached."""
+
+        # Set notification threshold smaller than days to expiration
+        Preference.objects.create(user=self.user, request_expiry_thresholds=[5])
+
+        with self.assertLogs('apps.allocations.tasks', level='DEBUG') as log:
+            self.assertFalse(should_notify_upcoming_expiration(self.user, self.request))
+            self.assertRegex(log.output[-1], '.*No notification threshold has been hit yet.')
+
+    def test_false_if_user_recently_joined(self) -> None:
+        """Verify the return value is `False` if the user recently joined."""
+
+        # Set account creation date after notification threshold
+        self.user.date_joined = datetime.now()
+        Preference.objects.create(user=self.user, request_expiry_thresholds=[15])
+
+        with self.assertLogs('apps.allocations.tasks', level='DEBUG') as log:
+            self.assertFalse(should_notify_upcoming_expiration(self.user, self.request))
+            self.assertRegex(log.output[-1], '.*User account created after notification threshold.')
+
+    @patch('apps.notifications.models.Notification.objects.filter')
+    def test_false_if_duplicate_notification(self, mock_notification_filter: Mock) -> None:
+        """Verify the return value is `False` if a notification has already been issued."""
+
+        # Set notification threshold equal to the days until expiration
+        Preference.objects.create(user=self.user, request_expiry_thresholds=[15])
+        mock_notification_filter.return_value.exists.return_value = True
+
+        with self.assertLogs('apps.allocations.tasks', level='DEBUG') as log:
+            self.assertFalse(should_notify_upcoming_expiration(self.user, self.request))
+            self.assertRegex(log.output[-1], '.*Notification already sent for threshold.')
+
+    def test_true_if_new_notification(self) -> None:
+        """Verify the return value is `True` if a notification threshold has been hit."""
+
+        # Set notification threshold equal to the days until expiration
+        Preference.objects.create(user=self.user, request_expiry_thresholds=[15])
+        self.assertTrue(should_notify_upcoming_expiration(self.user, self.request))
