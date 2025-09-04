24af07f2526f8030f9d625796d3fd07db140c922
Daniel Perrefort <djperrefort@gmail.com>
2025-09-04 18:37:17 -0400

    Updates entrypoint

--- conf/entrypoint.sh
+++ conf/entrypoint.sh
@@ -3,5 +3,5 @@
 set -e
 trap "kill 0" SIGINT SIGTERM
 
-nginx -g 'daemon off;' & # start nginx in background
+nginx # Start nginx in background
 exec keystone-api "$@" # Redirect commands to Keystone CLI
