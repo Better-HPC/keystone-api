5e707c40fcff9bf4df2684cc9aa54e1f7fba5599
Daniel Perrefort <djperrefort@gmail.com>
2025-10-18 20:11:16 -0400

    Updates tests

--- keystone_api/apps/users/tests/test_serializers/test_TeamSerializer.py
+++ keystone_api/apps/users/tests/test_serializers/test_TeamSerializer.py
@@ -1,8 +1,9 @@
 """Unit tests for the `TeamSerializer` class."""
 
 from django.test import TestCase
+from django.utils.text import slugify
 
-from apps.users.factories import MembershipFactory, TeamFactory, UserFactory
+from apps.users.factories import TeamFactory, UserFactory
 from apps.users.models import Membership
 from apps.users.serializers import TeamSerializer
 
@@ -47,6 +48,24 @@ class CreateMethod(TestCase):
 
         self.assertEqual(team_data["name"], team.name)
         self.assertEqual(0, team.membership.count())
+
+    def test_slug_created_dynamically(self) -> None:
+        """Verify dynamically generated slugs match to the team name."""
+
+        serializer = TeamSerializer(data={"name": "My Unique Team"})
+        self.assertTrue(serializer.is_valid(), serializer.errors)
+
+        team = serializer.create(serializer.validated_data)
+        self.assertEqual(slugify("My Unique Team"), team.slug)
+
+    def test_manual_slug_ignored(self) -> None:
+        """Verify manually specified slugs are ignored."""
+
+        serializer = TeamSerializer(data={"name": "My Unique Team", "slug": "wrong-slug"})
+        self.assertTrue(serializer.is_valid(), serializer.errors)
+
+        team = serializer.create(serializer.validated_data)
+        self.assertEqual(slugify("My Unique Team"), team.slug)
 
 
 class UpdateMethod(TestCase):
@@ -116,3 +135,24 @@ class UpdateMethod(TestCase):
         self.assertEqual(Membership.Role.OWNER, updated_team.membership.get(user=self.user1).role)
         self.assertEqual(Membership.Role.ADMIN, updated_team.membership.get(user=self.user2).role)
         self.assertEqual(Membership.Role.MEMBER, updated_team.membership.get(user=self.user3).role)
+
+    def test_slug_updated_dynamically(self) -> None:
+        """Verify slug values are updated to reflect the team name."""
+
+        update_data = {"name": "Completely New Team Name"}
+        serializer = TeamSerializer(instance=self.team)
+        updated_team = serializer.update(self.team, update_data)
+
+        self.assertEqual("Completely New Team Name", updated_team.name)
+        self.assertEqual(slugify("Completely New Team Name"), updated_team.slug)
+
+    def test_manual_slug_ignored(self) -> None:
+        """Verify manually specified slugs are ignored."""
+
+        update_data = {"name": "Slug Ignored Team", "slug": "wrong-slug"}
+        serializer = TeamSerializer(instance=self.team)
+        updated_team = serializer.update(self.team, update_data)
+
+        # Ensure the manually provided slug was not used
+        self.assertEqual(slugify("Slug Ignored Team"), updated_team.slug)
+        self.assertNotEqual("wrong-slug", updated_team.slug)
