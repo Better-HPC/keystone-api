f462158c1ebfd1d9fb32fdd449f1e9215a0f89b0
Daniel Perrefort <djperrefort@gmail.com>
2025-11-17 07:19:50 -0600

    Update task cleanup to manually manage task results

--- keystone_api/main/settings.py
+++ keystone_api/main/settings.py
@@ -351,8 +351,11 @@ PROMETHEUS_METRICS_EXPORT_PORT_RANGE = env.list('CONFIG_METRICS_PORTS', default=
 
 # Logging
 
+CELERY_RESULT_EXPIRES = None  # Disable Celery's internal log clean up
+
 LOG_REQ_RETENTION_SEC = env.int('LOG_REQ_RETENTION_SEC', timedelta(days=30).total_seconds())
 LOG_AUD_RETENTION_SEC = env.int('LOG_AUD_RETENTION_SEC', timedelta(days=30).total_seconds())
+LOG_TSK_RETENTION_SEC = env.int('LOG_TSK_RETENTION_SEC', timedelta(days=30).total_seconds())
 
 _default_log_dir = BASE_DIR / 'keystone.log'
 LOG_FILE_PATH = Path(os.getenv('LOG_APP_FILE', _default_log_dir))
