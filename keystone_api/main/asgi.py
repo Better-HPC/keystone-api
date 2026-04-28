0b911600fa92f7ddc23eb7b9d3a74679e7993dbc
Daniel Perrefort <djperrefort@gmail.com>
2026-04-28 19:01:19 -0400

    Sets default settings file in asgi.py

--- keystone_api/main/asgi.py
+++ keystone_api/main/asgi.py
@@ -1,7 +1,11 @@
 """Expose an ASGI callable as a module-level variable named `application`."""
 
+import os
+
 from django.conf import settings
 from django.core.asgi import get_asgi_application
 from servestatic import ServeStaticASGI
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'keystone_api.main.settings')
 
 application = ServeStaticASGI(get_asgi_application(), root=settings.STATIC_ROOT)
