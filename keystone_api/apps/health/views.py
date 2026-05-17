4375dd15c5b88323887e28a989c736fef7003aca
Daniel Perrefort <djperrefort@gmail.com>
2026-05-17 15:26:43 -0400

    Fix quotation typo

--- keystone_api/apps/health/views.py
+++ keystone_api/apps/health/views.py
@@ -242,7 +242,7 @@ class HealthCheckPrometheusView(BaseHealthCheckView):
 
         for row in results:
             metric_value = 200 if row["healthy"] else 500
-            lines.append(f"keystone_health_check_status{{check=\"{row["check"]}\"}} {metric_value:.1f}")
+            lines.append(f"keystone_health_check_status{{check=\"{row['check']}\"}} {metric_value:.1f}")
 
         lines += [
             "",
@@ -252,7 +252,7 @@ class HealthCheckPrometheusView(BaseHealthCheckView):
 
         for row in results:
             lines.append(
-                f"keystone_health_check_eval_time_seconds{{check=\"{row["check"]}\"}} {row["time_taken"]:.6f}"
+                f"keystone_health_check_eval_time_seconds{{check=\"{row['check']}\"}} {row['time_taken']:.6f}"
             )
 
         return HttpResponse(
